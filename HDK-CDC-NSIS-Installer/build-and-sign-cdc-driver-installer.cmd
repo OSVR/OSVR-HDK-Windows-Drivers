0d4f754f420d14b1110c89061450a2e6b9996d09
Ryan Pavlik <ryan@sensics.com>
2015-09-30 18:27:15 -0500

    Fix installer signing script to handle varied installer names.

--- HDK-CDC-NSIS-Installer/build-and-sign-cdc-driver-installer.cmd
+++ HDK-CDC-NSIS-Installer/build-and-sign-cdc-driver-installer.cmd
@@ -10,6 +10,6 @@ del *.exe >nul
 "%NSIS_DIR%\makensis" /DWDK_DIR="%WDK_DIR%" cdc-driver-installer.nsi
 @rem "%NSIS_DIR%\makensis" /DWDK_DIR="%WDK_DIR%" silent-cdc-driver-installer.nsi
 
-"%WDK_DIR%\bin\x86\Signtool" sign /v /n %CERTNAME% /t http://timestamp.verisign.com/scripts/timstamp.dll OSVR-HMD-CDC-Driver.exe
+"%WDK_DIR%\bin\x86\Signtool" sign /v /n %CERTNAME% /t http://timestamp.verisign.com/scripts/timstamp.dll OSVR-HMD-CDC-Driver*.exe
 
 popd
