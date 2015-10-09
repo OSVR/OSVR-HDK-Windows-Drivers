8fa21d6ae9a4a29e64f5820b53034195fbcdd593
Ryan Pavlik <ryan@sensics.com>
2015-10-09 10:36:23 -0500

    Bump and tweak versions - take 3 components for ourselves.
    1.2.1 for testing now.

--- Inf/update-timestamp-and-version.cmd
+++ Inf/update-timestamp-and-version.cmd
@@ -5,11 +5,11 @@ pushd "%~dp0"
 rem Remove old build products - cat invalidated by timestamp change.
 del *.cat *.htm > nul
 
-rem Update the date to today and the version to 6.4.whateveryouentered
-"%WDK_DIR%\bin\x86\stampinf" -f osvr_cdc.inf -d * -v 6.4.%DRIVER_VER%
+rem Update the date to today and the version to 7.whateveryouentered
+"%WDK_DIR%\bin\x86\stampinf" -f osvr_cdc.inf -d * -v 7.%DRIVER_VER%
 
-rem Update the date to today and the version to 1.0.whateveryouentered
-"%WDK_DIR%\bin\x86\stampinf" -f osvr_hdk_display.inf -d * -v 1.0.%DRIVER_VER%
+rem Update the date to today and the version to 1.whateveryouentered
+"%WDK_DIR%\bin\x86\stampinf" -f osvr_hdk_display.inf -d * -v 1.%DRIVER_VER%
 
 popd
 pause
