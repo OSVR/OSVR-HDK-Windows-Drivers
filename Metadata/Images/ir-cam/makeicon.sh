eb92c17be675446a0f25ff33c32769fb99d8a708
Ryan Pavlik <ryan@sensics.com>
2015-10-13 11:02:31 -0500

    Add source images for the device metadata icons

---
+++ Metadata/Images/ir-cam/makeicon.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+rm -rf output
+mkdir -p output/256
+mkdir -p output/16
+bigsrc=input/ircamera.png
+smallsrc=input/ircamera.png
+outname=HDK-IR-Camera.ico
+
+makeimg() {
+    infile=$1
+    size=$2
+    convert $infile -resize ${size}x${size} output/${size}.png
+}
+
+makeimg $bigsrc 256
+makeimg $smallsrc 48
+makeimg $smallsrc 32
+makeimg $smallsrc 24
+makeimg $smallsrc 16
+(
+    cd output
+    for fn in 48.png 32.png 24.png 16.png; do
+        convert $fn -channel A -threshold 75% +channel -background hotpink -alpha background  256/$fn.gif
+        convert $fn -channel A -threshold 75% +channel -background hotpink -alpha background  -colors 16 16/$fn.gif
+    done
+
+    convert *.png 256/* 16/*  $outname
+)
