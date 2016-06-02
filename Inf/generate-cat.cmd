e4167333c4d358b0789a53723f17e8e1fc79e700
Ryan Pavlik <ryan@sensics.com>
2016-06-02 16:18:11 -0500

    Update script to ensure signability for win10

--- Inf/generate-cat.cmd
+++ Inf/generate-cat.cmd
@@ -13,7 +13,7 @@ rem start *.inf.htm
 
 rem call "%WDK_DIR%\Tools\x86\chkinf\chkinf" *.inf
 
-"%WDK_DIR%\bin\x86\inf2cat" /driver:. /os:6_3_X86,6_3_X64,6_3_ARM,Server6_3_X64,8_X64,8_X86,8_ARM,Server8_X64,Server2008R2_X64,Server2008R2_IA64,7_X64,7_X86,Server2008_X64,Server2008_IA64,Server2008_X86,Vista_X64,Vista_X86,Server2003_X64,Server2003_IA64,Server2003_X86,XP_X64,XP_X86
+"%WDK_DIR%\bin\x86\inf2cat" /driver:. /os:10_X86,10_X64,6_3_X86,6_3_X64,6_3_ARM,Server6_3_X64,8_X64,8_X86,8_ARM,Server8_X64,Server2008R2_X64,Server2008R2_IA64,7_X64,7_X86,Server2008_X64,Server2008_IA64,Server2008_X86,Vista_X64,Vista_X86,Server2003_X64,Server2003_IA64,Server2003_X86,XP_X64,XP_X86
 
 rem pnputil -a *.inf
 rem pnputil -i -a *.inf
