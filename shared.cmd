f727cace1540bb83bcb0a07e834e2a1c2e7d5a17
Ryan Pavlik <ryan@sensics.com>
2016-06-02 15:37:04 -0500

    Bump version for release.

--- shared.cmd
+++ shared.cmd
@@ -1,7 +1,7 @@
 @rem Batch file containing shared environment variable definitions used by various scripts.
 
 @rem This should be updated for each release
-@set DRIVER_VER=1.2.6
+@set DRIVER_VER=1.2.7
 
 @rem These are the default values, don't edit here, override - see below.
 @set ROOTDIR=%~dp0
